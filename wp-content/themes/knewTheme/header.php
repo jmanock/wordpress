e3712e22d37462d775818afef93c618637f66bc3
Jon Manock <jonmanock@gmail.com>
2017-08-11 15:47:00 -0400

    working on customs

---
+++ wp-content/themes/knewTheme/header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Blog Template for bootstrap</title>
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
+  <?php wp_head();?>
+</head>
